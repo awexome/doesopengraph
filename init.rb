e22aa57cbd3be669738f43f14100c7010392850d
Colin McCloskey <colin.mccloskey@gmail.com>
2011-02-05 00:52:22 -0500

    change some nomenclature

--- init.rb
+++ init.rb
@@ -1,11 +1,5 @@
 # AWEXOME
 # DoFacebook - initializer
 
-require 'do_facebook'
-
-require 'utils'
-require 'do_document_fields'
-ActiveRecord::Base.class_eval do
-  include Awexome::Do::DocumentFields
-end
+require 'doopengraph'
 
--- lib/do_facebook.rb
+++ lib/do_facebook.rb
@@ -1,48 +1,49 @@
 # AWEXOME
-# DoFacebook - meat and potatoes
+# DoOpenGraph - meat and potatoes
 
-class DoFacebook
+module DoOpenGraph
+  class GraphAPI
 
-  require "net/http"
-  require "uri"
-  require "json"
+    require "net/http"
+    require "uri"
+    require "json"
   
-  HTTP_GRAPH_ENDPOINT = "http://graph.facebook.com/"
-  HTTPS_GRAPH_ENDPOINT = "http://graph.facebook.com/"
+    HTTP_GRAPH_ENDPOINT = "http://graph.facebook.com/"
+    HTTPS_GRAPH_ENDPOINT = "http://graph.facebook.com/"
   
-  def initialize(acctok=nil)
-    @access_token = acctok
-  end
-  
-  def node(id, connection=nil, opts={})
-    opts = connection.dup and connection = nil if connection.is_a?(Hash)
-    opts = {:access_token=>@access_token}.merge(opts) if @access_token
-    nodepath = connection.nil? ? File.join(id) : File.join(id, connection)
-    nodepath += "?#{opts.collect{|k,v| "#{k}=#{v}"}.join("=")}" unless opts.nil? || opts.empty?
-    base = URI.parse(HTTP_GRAPH_ENDPOINT)
-    begin
-      response = Net::HTTP.start(base.host, base.port) {|http| http.get(File.join("/",nodepath)) }
-      return data = JSON.parse(response.body)
-    rescue JSON::ParserError => jsone
-      puts "JSON not returned for the requested node" and return nil
-    end    
-  end
-  alias_method :get, :node
-  
-  
-  def update(id, connection, opts={})
-    opts = {:access_token=>@access_token}.merge(opts) if @access_token
-    nodepath = File.join(id, connection)
-    begin
-      response = Net::HTTP.post_form( URI.parse(File.join(HTTPS_GRAPH_ENDPOINT,nodepath)), opts )
-    # rescue Exception => e
-    #   return nil
+    def initialize(acctok=nil)
+      @access_token = acctok
     end
-  end
-  alias_method :post, :update
+  
+    def node(id, connection=nil, opts={})
+      opts = connection.dup and connection = nil if connection.is_a?(Hash)
+      opts = {:access_token=>@access_token}.merge(opts) if @access_token
+      nodepath = connection.nil? ? File.join(id) : File.join(id, connection)
+      nodepath += "?#{opts.collect{|k,v| "#{k}=#{v}"}.join("=")}" unless opts.nil? || opts.empty?
+      base = URI.parse(HTTP_GRAPH_ENDPOINT)
+      begin
+        response = Net::HTTP.start(base.host, base.port) {|http| http.get(File.join("/",nodepath)) }
+        return data = JSON.parse(response.body)
+      rescue JSON::ParserError => jsone
+        puts "JSON not returned for the requested node" and return nil
+      end    
+    end
+    alias_method :get, :node
+  
+  
+    def update(id, connection, opts={})
+      opts = {:access_token=>@access_token}.merge(opts) if @access_token
+      nodepath = File.join(id, connection)
+      begin
+        response = Net::HTTP.post_form( URI.parse(File.join(HTTPS_GRAPH_ENDPOINT,nodepath)), opts )
+      # rescue Exception => e
+      #   return nil
+      end
+    end
+    alias_method :post, :update
 
-    
-end #DoFacebook
+  end # GraphAPI    
+end # DoOpenGraph
       
 
 
