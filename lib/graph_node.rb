568e69fcc323d9ca758388838e4887155252c7c9
Colin McCloskey <colin.mccloskey@gmail.com>
2011-02-05 03:02:59 -0500

    Nodes are subclassed from Responses

--- lib/graph_node.rb
+++ lib/graph_node.rb
@@ -5,26 +5,13 @@
 #
 
 module DoOpenGraph
-  class GraphNode
+  class GraphNode < GraphResponse
 
-    attr_reader :content, :api
-
-    # Build a Node object from request content and store the api
-    def initialize(content, api=nil)
-      @content = content.is_a?(Hash) ? content : Hash.new
-      @api = api
-    end
-    
     # Fetch an updated view of this node
     def reload
       up = api.node(self.id)
       @content = up.content
       self
-    end
-  
-    # Use secret patching to provide accessors to connections within node
-    def method_missing(m, *args, &block)
-      content.include?(m.to_s) ? content[m.to_s] : nil
     end
 
   end # GraphNode    
--- lib/graph_response.rb
+++ lib/graph_response.rb
@@ -9,7 +9,7 @@ module DoOpenGraph
 
     attr_reader :content, :api
 
-    # Build a Node object from request content and store the api
+    # Build a Response object from request content and store the api
     def initialize(content, api=nil)
       @content = content.is_a?(Hash) ? content : Hash.new
       @api = api
