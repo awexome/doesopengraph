b2c03397407bf6cbd5ca2e825854943fbc1b05bc
Graham Powrie <graham@developmentnow.com>
2011-08-09 12:25:22 -0700

    took out path from #nodes in error response

--- lib/doesopengraph/graph_api.rb
+++ lib/doesopengraph/graph_api.rb
@@ -32,7 +32,7 @@ module DoesOpenGraph
             data = JSON.parse(response.body)
             responses << GraphNode.new(data, self)
           rescue JSON::ParserError => jsone
-            raise "Invalid JSON or poorly formed JSON returned for #{path}" and return nil
+            raise "Invalid JSON or poorly formed JSON returned" and return nil
           end
         end
         
@@ -64,6 +64,7 @@ module DoesOpenGraph
         # Make a request and parse JSON result:
         begin
           response = Typhoeus::Request.get(href, :params=>params)
+          debugger
           data = JSON.parse(response.body)
           return GraphNode.new(data, self)
         rescue JSON::ParserError => jsone
@@ -85,9 +86,10 @@ module DoesOpenGraph
       href = File.join(base, path)
 
       # Make our POST request and check the results:
-      begin
+      begin        
         response = Typhoeus::Request.post(href, :params=>params)
         data = JSON.parse(response.body)
+        
         return GraphResponse.new(data)
       rescue JSON::ParserError => jsone
         # A JSON.parse on "true" triggers an error, so let's build it straight from body:
