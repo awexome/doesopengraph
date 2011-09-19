bf857ab536df07cb6e01c08358002055360966b7
Colin McCloskey <colin.mccloskey@gmail.com>
2011-09-19 17:29:39 -0400

    remove GraphNode internals

--- lib/doesopengraph/graph_node.rb
+++ lib/doesopengraph/graph_node.rb
@@ -6,35 +6,6 @@
 
 module DoesOpenGraph
   class GraphNode < GraphResponse
-
-    # Fetch an updated view of this node
-    def reload
-      up = api.node(self.id)
-      @content = up.content
-      self
-    end
-    
-    # Introspect on the connections available to this node
-    def introspect
-      api.node(self.id, nil, :metadata=>1)
-    end
-    
-    # Delete this node from the graph
-    def delete
-      api.delete(self.id)
-    end
-    
-    # Like this node, if supported
-    def like
-      api.post(self.id, "likes")
-    end
-    
-    # Unlike this node, if supported
-    def unlike
-      api.delete(self.id, "likes")
-    end
-    
-
   end # GraphNode    
 end # DoesOpenGraph
       
